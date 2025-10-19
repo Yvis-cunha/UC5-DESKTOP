20d9a392494fccd9f9165269bfd88281fd7d282f
Yvis-Cunha <ivisigor3@gmail.com>
2025-10-19 18:03:06 -0300

    Atividade electron1

--- app/aula1/main.js
+++ app/aula1/main.js
@@ -3,9 +3,14 @@ import {app, BrowserWindow}from 'electron'
 const criarjanela = () =>{
     const janela = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        
+        
+        
     })
     janela.loadFile('index.html')
+    janela.setMenu(null); // remover o menu dentro da interface
+    
 }
 
 
